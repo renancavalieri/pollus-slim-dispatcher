47d862d6024940164e489e636f3629700796f408
Renan Cavalieri <renan@tecdicas.com>
2018-11-18 23:13:46 -0200

    Update MiddlewareInterface.php

--- src/Middlewares/MiddlewareInterface.php
+++ src/Middlewares/MiddlewareInterface.php
@@ -25,6 +25,7 @@ interface MiddlewareInterface
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @param callable $next
+     * @return ResponseInterface
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next);
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next) : ResponseInterface;
 }
